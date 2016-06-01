4cf77ea6d8d0aa064fe32e0b6c4e5bab2c73977c
Willian Itiho Amano <itihoitiho@gmail.com>
2016-06-01 15:29:34 -0400

    Expanção para limite de CPU também e duas opções de script

--- Remove-VMs-Memory-Limit.ps1
+++ Remove-VMs-Memory-Limit.ps1
@@ -1,6 +1,6 @@
 ﻿#########################################################################################
-# Purpose : Remove VMs Memory Limit                                                     #
-# Descrição: Remove a limitação de memória das VMs                                      #
+# Purpose : Remove VMs Memory and CPU Limit                                             #
+# Descrição: Remove a limitação de memória e CPU das VMs                                #
 # Version: 1.0                                                                          #
 # Author  : Willian Itiho Amano - itihoitiho@gmail.com                                  #
 # Release Date: 01/06/2016                                                              # 
@@ -12,7 +12,20 @@ Add-PSSnapin VMware.VimAutomation.Core
 
 Connect-VIServer
 
-
+##Option 1
+#Opção 1
 Get-VM | Foreach-Object -Process { 
     Tee-Object -InputObject $_ -Variable Temp | Get-VMResourceConfiguration | where {$_.MemLimitMB -ne ‘-1’}  
 } | Set-VMResourceConfiguration -MemLimitMB $null 
+
+
+Get-VM | Foreach-Object -Process { 
+    Tee-Object -InputObject $_ -Variable Temp | Get-VMResourceConfiguration | where {$_.CPULimitMhz -ne ‘-1’}  
+} | Set-VMResourceConfiguration -CPULimitMhz $null 
+
+
+
+#Option 2
+#Opção 2
+#Get-VM | Get-VMResourceConfiguration | Where-Object { $ _ .MemLimitMB -ne ' -1 ' } | Set-VMResourceConfiguration -MemLimitMB $ null
+#Get-VM | Get-VMResourceConfiguration | Where-Object { $ _ .CpuLimitMhz -ne ' -1 ' } | Set-VMResourceConfiguration -CPULimitMhz $ null
