6c8e65cb151a1b5e1b81c6581f62792db8e2e95e
Willian Itiho Amano <itihoitiho@gmail.com>
2016-06-01 11:04:01 -0400

    Remove Floppy Drive das máquinas desligadas

---
+++ Remove-Floppy-Drive.ps1
@@ -0,0 +1,17 @@
+﻿#########################################################################################
+# Purpose : Remove Floppy Drive                                                         #
+# Descrição: Remove os drivers de disquete                                              #
+# Version: 1.0                                                                          #
+# Author  : Willian Itiho Amano - itihoitiho@gmail.com                                  #
+# Release Date: 01/06/2016                                                              # 
+#########################################################################################
+
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+Import-Module VMware.VimAutomation.Vds  
+Add-PSSnapin VMware.VimAutomation.Core
+
+
+
+$off = Get-VM | where {$_.powerstate -eq "PoweredOff"}
+$floppy = Get-FloppyDrive -VM $off
+Remove-FloppyDrive -Floppy $floppy -Confirm:$false
