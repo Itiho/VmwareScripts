3eb7a9756ceacb912a2ed81ed8c0b7b8f831d00c
Willian Itiho Amano <itihoitiho@gmail.com>
2016-06-03 09:47:09 -0400

    Script para atualizar VMs Windows sem reboot

---
+++ vmware-tools-update-without-reboot.ps1
@@ -0,0 +1,25 @@
+﻿#########################################################################################
+# Purpose : VMware Tools Update in Windows VMs without reboot                           #
+# Descrição: Atualiza VMware Tools em VMs Windows sem reiniciar                         #
+# Version: 1.0                                                                          #
+# Author  : Willian Itiho Amano - itihoitiho@gmail.com                                  #
+# Release Date: 01/06/2016                                                              # 
+#########################################################################################
+
+
+#Carrega o Snapin do VMWare PowerCLI
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+Import-Module VMware.VimAutomation.Vds  
+Add-PSSnapin VMware.VimAutomation.Core
+
+$vms=GET-VM |  Where-Object {$_.PowerState -eq "PoweredOn" } 
+ForEach($vm in $vms) {
+    #Get-VMguest -VM $vm | Where-Object {$_.OSFullName -like "*Microsoft*"}
+    if ($vm.ExtensionData.Guest.GuestFullName -like '*Microsoft*') { 
+
+        if ($vm.ExtensionData.Guest.ToolsVersionStatus -ne "guestToolsCurrent"){
+            Get-VM $vm | Update-Tools -NoReboot -RunAsync
+        }
+    }
+}
+
