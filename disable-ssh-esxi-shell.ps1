204c1072eec7f4585d128be1f866823f422b4c11
Willian Itiho Amano <itihoitiho@gmail.com>
2016-06-16 11:42:16 -0400

    Adicionado esxi Shell

--- disable-ssh-esxi-shell.ps1
+++ disable-ssh-esxi-shell.ps1
@@ -1,6 +1,6 @@
 ﻿#########################################################################################
-# Purpose : Disable and stop ssh service                                                #
-# Descrição: Desabilita e para o serviço SSH                                            #
+# Purpose : Disable and stop SSH and ESXi Shell services                                #
+# Descrição: Desabilita e para os serviços SSH e ESXi SHell                             #
 # Version: 1.0                                                                          #
 # Author  : Willian Itiho Amano - itihoitiho@gmail.com                                  #
 # Release Date: 16/06/2016                                                              # 
@@ -17,4 +17,6 @@ $vmhosts = Get-VMHost
 foreach ($vmhost in $vmhosts ) {
     Get-VmHostService -VMHost $vmhost | Where-Object {$_.key -eq "TSM-SSH"} |  Set-VMHostService -policy "off"
     Get-VmHostService -VMHost $vmhost | Where-Object {$_.key -eq "TSM-SSH"} | Stop-VMHostService -Confirm:$false
+    Get-VmHostService -VMHost $vmhost | Where-Object {$_.key -eq "TSM"} |  Set-VMHostService -policy "off"
+    Get-VmHostService -VMHost $vmhost | Where-Object {$_.key -eq "TSM"} | Stop-VMHostService -Confirm:$false
 }
