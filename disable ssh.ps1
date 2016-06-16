0a609ce39aaf8cd29ef52ec5809aca979f18992a
Willian Itiho Amano <itihoitiho@gmail.com>
2016-06-16 08:54:26 -0400

    Script para desabilitar e parar SSH

---
+++ disable ssh.ps1
@@ -0,0 +1,20 @@
+﻿#########################################################################################
+# Purpose : Disable and stop ssh service                                                #
+# Descrição: Desabilita e para o serviço SSH                                            #
+# Version: 1.0                                                                          #
+# Author  : Willian Itiho Amano - itihoitiho@gmail.com                                  #
+# Release Date: 16/06/2016                                                              # 
+#########################################################################################
+
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+Add-PSSnapin VMware.VimAutomation.Core
+
+Connect-VIServer
+
+
+$vmhosts = Get-VMHost 
+
+foreach ($vmhost in $vmhosts ) {
+    Get-VmHostService -VMHost 10.0.10.60 | Where-Object {$_.key -eq "TSM-SSH"} |  Set-VMHostService -policy "off"
+    Get-VmHostService -VMHost $vmhost | Where-Object {$_.key -eq "TSM-SSH"} | Stop-VMHostService -Confirm:$false
+}
